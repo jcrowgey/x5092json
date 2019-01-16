2280b8f1e677642a98b6d9dafe005fd2e6de49e3
Joshua Crowgey <jcrowgey@domaintools.com>
2019-01-16 13:41:14 -0800

    bumping version for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='x5092json',
-      version='1.0.0',
+      version='1.0.1',
       description='x5092json Utility',
       author='Joshua Crowgey',
       author_email='jcrowgey@uw.edu',
